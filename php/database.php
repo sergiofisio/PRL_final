2d22a5512af4607c9a986d46be486196e25751d5
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-06-19 18:22:49 -0300

    contato

--- php/database.php
+++ php/database.php
@@ -1,30 +1,9 @@
 <?php
-// Carregar o conteúdo do arquivo JSON
 $jsonFile = '../database/products.json';
 $jsonData = file_get_contents($jsonFile);
 
-// Decodificar o JSON para um array associativo
 $products = json_decode($jsonData, true);
 
-// Função para exibir os produtos
-function exibirProdutos($products)
-{
-    foreach ($products as $categoria) {
-        echo "<h2>Categoria: " . htmlspecialchars($categoria['categoria']) . "</h2>";
-        if (isset($categoria['tipo'])) {
-            echo "<h3>Tipo: " . htmlspecialchars($categoria['tipo']) . "</h3>";
-        }
-        foreach ($categoria['produtos'] as $produto) {
-            echo "<div>";
-            echo "<strong>Nome:</strong> " . htmlspecialchars($produto['nome']) . "<br>";
-            echo "<strong>Descrição:</strong> " . htmlspecialchars($produto['descrição']) . "<br>";
-            echo "<strong>Categoria:</strong> " . htmlspecialchars($produto['categoria']) . "<br>";
-            echo "<strong>Imagem:</strong> <img src='" . htmlspecialchars($produto['img']) . "' alt='" . htmlspecialchars($produto['nome']) . "'><br>";
-            echo "<strong>Valor:</strong> " . htmlspecialchars($produto['valor']) . "€<br>";
-            echo "</div><br>";
-        }
-    }
-}
+header('Content-Type: application/json');
 
-// Exibir os produtos
-exibirProdutos($products);
+echo json_encode($products);
