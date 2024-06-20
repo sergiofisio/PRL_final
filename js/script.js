29cb14ed7b481ab89fa2d96b3d4d5560c884be50
sergiobastosfisio@gmail.com <sergiobastosfisio@gmail.com>
2024-06-20 10:49:11 -0300

    modal shop

--- js/script.js
+++ js/script.js
@@ -1,12 +1,33 @@
 const aside = document.querySelector("aside");
 const section = document.querySelector("section");
 const response = document.querySelector("#response");
+const cart = document.querySelector(".cartValue");
+const valueCart = document.getElementById("valueCart");
+const modal = document.querySelector(".modalShop");
+const closedButton = document.querySelector(".closedButton");
+
+closedButton.addEventListener("click", function () {
+  modal.classList.add("hidden");
+});
 
 const categories = new Set();
 let produtos = [];
+let render = localStorage.getItem("render") || false;
+
+function getCartProducts() {
+  const numberProductsInCart = JSON.parse(localStorage.getItem("cart")) || [];
+
+  valueCart.textContent = numberProductsInCart.length;
+}
 
 function renderProducts(products) {
+  getCartProducts();
   section.innerHTML = "";
+
+  localStorage.setItem("render", true);
+  render = localStorage.getItem("render");
+  const cartItens = JSON.parse(localStorage.getItem("cart")) || [];
+
   for (const product of products) {
     const div = document.createElement("div");
     const img = document.createElement("img");
@@ -33,14 +54,48 @@ function renderProducts(products) {
         return alert("Esse item ja foi adicionado ao carrinho");
       }
 
-      cart.push(product);
+      cart.push({ ...product, quantidade: 1 });
 
       localStorage.setItem("cart", JSON.stringify(cart));
+      getCartProducts();
     });
 
     section.appendChild(div);
     div.append(img, h2, h3, button);
   }
+  cart.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    cartItens.forEach((item) => {
+      console.log({ item });
+
+      const tr = document.createElement("tr");
+      const tdImg = document.createElement("td");
+      const img = document.createElement("img");
+      const tdName = document.createElement("td");
+      const tdQtt = document.createElement("td");
+      const sum = document.createElement("h3");
+      const minus = document.createElement("h3");
+      const tdPrice = document.createElement("td");
+      img.src = item.img;
+      img.alt = `imagem ${item.nome}`;
+      tdImg.classList.add("tableImg");
+
+      tdImg.appendChild(img);
+      tdName.textContent = item.nome;
+      tdName.classList.add("tableName");
+      sum.textContent = "+";
+      minus.textContent = "-";
+      tdQtt.append(sum, (tdQtt.textContent = item.quantidade), minus);
+      tdQtt.classList.add("tableQtt");
+      tdPrice.textContent = item.valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+      tdPrice.classList.add("tablePrice");
+      tr.append(tdImg, tdName, tdQtt, tdPrice);
+      document.querySelector("tbody").appendChild(tr);
+    });
+  });
 }
 
 $.ajax({
@@ -48,99 +103,103 @@ $.ajax({
   dataType: "json",
   type: "GET",
   contentType: "application/json",
-
   beforeSend: function () {
-    response.textContent = "Carregando...";
+    if (!render) {
+      response.textContent = "Carregando...";
+    }
   },
 })
 
   .done(function (data) {
-    setTimeout(() => {
-      response.textContent = "";
+    setTimeout(
+      () => {
+        response.textContent = "";
 
-      for (const products of data) {
-        try {
-          categories.add(products.categoria);
-          produtos.push(...products.produtos);
-        } catch (error) {
-          console.log({ error });
+        for (const products of data) {
+          try {
+            categories.add(products.categoria);
+            produtos.push(...products.produtos);
+          } catch (error) {
+            console.log({ error });
+          }
         }
-      }
-      renderProducts(produtos);
-
-      categories.forEach((category) => {
-        const categoryDiv = document.createElement("div");
-        const subcategoryDiv = document.createElement("div");
-        const h2 = document.createElement("h2");
-        const img = document.createElement("img");
-        h2.textContent = category;
-        h2.classList.add(
-          "m-0",
-          "text-capitalize",
-          "fs-5",
-          "pointer",
-          "red",
-          "gap-3",
-          "d-flex"
-        );
-        img.src = "../../img/icons/arrowsDown.svg";
-        img.alt = "seta para baixo";
-        h2.appendChild(img);
-        categoryDiv.appendChild(h2);
-        aside.appendChild(categoryDiv);
-        subcategoryDiv.style.display = "none";
-
-        h2.addEventListener("click", function () {
-          const isCurrentlyVisible = subcategoryDiv.style.display === "block";
-
-          subcategoryDiv.style.display = isCurrentlyVisible ? "none" : "block";
-        });
-
-        $.ajax({
-          url: "../../database/categories.json",
-          dataType: "json",
-          type: "GET",
-          contentType: "application/json",
-        })
-          .done(function (subcategories) {
-            console.log({ subcategories });
-
-            let categories = subcategories[0].categorias;
-            for (let i = 0; i < categories.length; i++) {
-              if (categories[i].tipo === category) {
-                const h2 = document.createElement("h2");
-                h2.textContent = categories[i].descrição;
-                h2.classList.add(
-                  "m-0",
-                  "text-capitalize",
-                  "fs-6",
-                  "pointer",
-                  "red"
-                );
-
-                h2.addEventListener("click", function () {
-                  const filterProducts = produtos.filter(
-                    (produto) => produto.categoria === categories[i].id
-                  );
-                  renderProducts(filterProducts);
-                });
-
-                subcategoryDiv.appendChild(h2);
-                categoryDiv.appendChild(subcategoryDiv);
-              }
-            }
-          })
-          .fail(function (_, textStatus, errorThrown) {
-            console.error("Error:", textStatus, errorThrown);
-          });
-      });
-      const button = document.createElement("button");
-      button.textContent = "Mostrar todos os produtos";
-      button.addEventListener("click", function () {
         renderProducts(produtos);
-      });
-      aside.appendChild(button);
-    }, 2000);
+
+        categories.forEach((category) => {
+          const categoryDiv = document.createElement("div");
+          const subcategoryDiv = document.createElement("div");
+          const h2 = document.createElement("h2");
+          const img = document.createElement("img");
+          h2.textContent = category;
+          h2.classList.add(
+            "m-0",
+            "text-capitalize",
+            "fs-5",
+            "pointer",
+            "red",
+            "gap-3",
+            "d-flex"
+          );
+          img.src = "../../img/icons/arrowsDown.svg";
+          img.alt = "seta para baixo";
+          h2.appendChild(img);
+          categoryDiv.appendChild(h2);
+          aside.appendChild(categoryDiv);
+          subcategoryDiv.style.display = "none";
+
+          h2.addEventListener("click", function () {
+            const isCurrentlyVisible = subcategoryDiv.style.display === "block";
+
+            subcategoryDiv.style.display = isCurrentlyVisible
+              ? "none"
+              : "block";
+          });
+
+          $.ajax({
+            url: "../../database/categories.json",
+            dataType: "json",
+            type: "GET",
+            contentType: "application/json",
+          })
+            .done(function (subcategories) {
+              let categories = subcategories[0].categorias;
+              for (let i = 0; i < categories.length; i++) {
+                if (categories[i].tipo === category) {
+                  const h2 = document.createElement("h2");
+                  h2.textContent = categories[i].descrição;
+                  h2.classList.add(
+                    "m-0",
+                    "text-capitalize",
+                    "fs-6",
+                    "pointer",
+                    "red"
+                  );
+
+                  h2.addEventListener("click", function () {
+                    const filterProducts = produtos.filter(
+                      (produto) => produto.categoria === categories[i].id
+                    );
+                    renderProducts(filterProducts);
+                  });
+
+                  subcategoryDiv.appendChild(h2);
+                  categoryDiv.appendChild(subcategoryDiv);
+                }
+              }
+            })
+            .fail(function (_, textStatus, errorThrown) {
+              console.error("Error:", textStatus, errorThrown);
+            });
+        });
+        const button = document.createElement("button");
+        button.textContent = "Mostrar todos os produtos";
+        button.addEventListener("click", function () {
+          renderProducts(produtos);
+        });
+        aside.appendChild(button);
+      },
+      render ? 0 : 2000
+    );
   })
   .fail(function (_, textStatus, errorThrown) {
     console.error("Error:", textStatus, errorThrown);
